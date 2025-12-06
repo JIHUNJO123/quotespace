6749900044f76b3ec33c88b544ceb3343d9c6e34
jihunjo <jihunjo@example.com>
2025-12-06 15:50:20 +0900

    Change bundle ID to com.jihunjo.quoteenglishdaily

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.daily_quotes"
+    namespace = "com.jihunjo.quoteenglishdaily"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.daily_quotes"
+        applicationId = "com.jihunjo.quoteenglishdaily"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
